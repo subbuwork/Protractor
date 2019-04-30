530e96f9f212295b331ae6b2148aaad0a89e4840
SUBBARAO BANDI <subbarao.bandi@yahoo.com>
2019-04-30 07:46:05 -0400

    Add files via upload

---
+++ spec.js
@@ -0,0 +1,8 @@
+//import {browser} from "protractor";
+
+describe('demo',function(){
+    it('demo 123',function(){
+        browser.get('http://juliemr.github.io/protractor-demo/');
+       
+    })
+});
